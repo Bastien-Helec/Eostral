909c2adc9b6a3177268467423c0fa6869071c0b1
Super User <root@device-31.home>
2025-05-20 18:52:03 +0000

    fix: -On peut enfin faire fonctionner la libs en separer de la partie docker - L'exemple présent dans docker/prod en temoigne

--- docker/prod/index.php
+++ docker/prod/index.php
@@ -1,7 +1,27 @@
 <?php
+
+require_once "./../libs/Model/PHP/PAGES/Fields.php";
+require_once "./../libs/View/Fields.php";
+
+$test = new Glob_Fields('cc','cc','h1','Coucou');
+
+$mytest= $test->gen_balise();
+
+echo render_Glob_Fields($mytest);
+
+$banner = new Glob_Fields(
+    'banner', 'banner', 'div', [
+        new Glob_Fields('titre', 'titre', 'h1', 'Bienvenue'),
+        new Glob_Fields('soustitre', 'soustitre', 'p', 'Portail interne')
+    ]
+);
+
+echo render_Glob_Fields($banner->gen_balise());
+
+
 ?>
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,4 +31,4 @@
 <body>
     <h1> Coucou je suis de la pre-prod</h1>
 </body>
-</html>
+</html> -->
